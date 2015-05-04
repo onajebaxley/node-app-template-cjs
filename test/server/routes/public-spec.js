c883c9b7699eb732543008c40dd8ec96da7ea6c5
vamship <vamshi.ponnapalli@gmail.com>
2015-05-04 11:09:23 -0400

    Additional annotation on tests

--- test/server/routes/public-spec.js
+++ test/server/routes/public-spec.js
@@ -13,19 +13,21 @@ describe('[public route]', function() {
         return _path.join(MOUNT_PATH, path);
     }
 
-    it('should show the application status when invoked', function(done) {
-        request.get(_getPath('/__status'))
-            .set('Accept', 'application/json')
-            .expect(200)
-            .end(function(err, res) {
-                expect(err).to.be.null;
+    describe('GET /__status', function() {
+        it('should show the application status when invoked', function(done) {
+            request.get(_getPath('/__status'))
+                .set('Accept', 'application/json')
+                .expect(200)
+                .end(function(err, res) {
+                    expect(err).to.be.null;
 
-                var payload = res.body;
-                expect(payload).to.be.defined;
-                expect(payload.app).to.equal('node-app-template-cjs');
-                expect(payload.version).to.be.a('string');
-                expect(payload.timestamp).to.be.a('number');
-                done();
-            });
+                    var payload = res.body;
+                    expect(payload).to.be.defined;
+                    expect(payload.app).to.equal('node-app-template-cjs');
+                    expect(payload.version).to.be.a('string');
+                    expect(payload.timestamp).to.be.a('number');
+                    done();
+                });
+        });
     });
 });
