8bf07c836c9690875a88a530ff680f923833c7ba
vamship <vamshi.ponnapalli@gmail.com>
2015-05-06 08:34:20 -0400

    Node environment for all server tests set to "test"

--- Gruntfile.js
+++ Gruntfile.js
@@ -593,17 +593,16 @@ module.exports = function(grunt) {
          */
         express: {
             options: {
-                debug: true
+                debug: true,
+                node_env: 'test'
             },
             dev: {
                 options: {
-                    node_env: 'development',
                     script: SERVER.getChildPath('server.js')
                 }
             },
             build: {
                 options: {
-                    node_env: 'test',
                     script: SERVER_BUILD.getChildPath('server.js')
                 }
             }
