e6a8f30cb7fa3b7313859244fed781948c7216a1
vamship <vamshi.ponnapalli@gmail.com>
2016-01-25 17:02:28 -0500

    Added error route tests (incomplete)

--- test/http/error-routes-spec.js
+++ test/http/error-routes-spec.js
@@ -15,7 +15,7 @@ _chai.use(require('chai-as-promised'));
 var expect = require('chai').expect;
 var _assertionHelper = _wyskndTest.assertionHelper;
 
-describe('[public routes]', function() {
+describe('[error routes]', function() {
     var MOUNT_PATH = '/';
     var request = _supertest(_config.baseUrl);
 
@@ -23,16 +23,16 @@ describe('[public routes]', function() {
         return _path.join(MOUNT_PATH, path);
     }
 
-    describe('[GET /]', function() {
+    describe('[authentication failed error]', function() {
         var path = null;
 
         beforeEach(function() {
-            path = _getPath('/');
+            path = _getPath('/app/');
         });
 
-        it('should return an HTTP 200 code with correct response headers when invoked', function(done) {
+        xit('should return an HTTP 401 code with correct response headers when invoked', function(done) {
             request.get(path)
-                .expect(200)
+                .expect(401)
                 .expect('content-type', /text\/html/)
                 .end(function(err, res) {
                     expect(err).to.be.null;
@@ -41,39 +41,22 @@ describe('[public routes]', function() {
         });
     });
 
-    describe('[GET /__status]', function() {
+    describe('[resource not found error]', function() {
         var path = null;
 
         beforeEach(function() {
-            path = _getPath('/__status');
+            path = _getPath('/missing/resource');
         });
 
-        it('should return an HTTP 200 code with correct response headers when invoked', function(done) {
+        it('should return an HTTP 404 code with correct response headers when invoked', function(done) {
             request.get(path)
-                .expect(200)
-                .expect('content-type', /application\/json/)
+                .expect(404)
+                .expect('content-type', /text\/html/)
                 .end(function(err, res) {
                     expect(err).to.be.null;
-                    done();
-                });
-        });
-
-        it('should respond with the correct application status payload when invoked', function(done) {
-            var startTime = Date.now();
-            request.get(path)
-                .end(function(err, res) {
-                    expect(err).to.be.null;
-
-                    var payload = res.body;
-                    var versionPattern = new RegExp(_packageJson.version);
-
-                    expect(payload).to.be.defined;
-                    expect(payload.app).to.equal(_packageJson.name);
-                    expect(payload.version).to.match(versionPattern);
-                    expect(payload.timestamp).to.be.a('number');
-                    expect(payload.timestamp).to.be.within(startTime, Date.now());
                     done();
                 });
         });
     });
+
 });
