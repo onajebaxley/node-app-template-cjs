4c786831b68c11feb0841901239ab74d41724569
vamship <vamshi.ponnapalli@gmail.com>
2016-02-05 10:11:29 -0500

    Added routine to initialize client side app settings

--- client/js/app.js
+++ client/js/app.js
@@ -1,6 +1,8 @@
 /**
  * Entry point for the javascript application. Initializes all required modules
  * and ui routes.
+ *
+ * @module app
  */
 'use strict';
 
@@ -9,11 +11,14 @@ var angular = require('angular');
 var angularMaterial = require('angular-material');
 var uiRouter = require('angular-ui-router');
 
+var moduleName = 'app';
+
 var routes = require('./routes');
+var icons = require('./icons');
+
 var templates = require('./templates');
 var coreModule = require('./app.core');
 var authModule = require('./app.auth');
-var moduleName = 'app';
 
 angular.module(moduleName, [
     templates,
@@ -21,7 +26,21 @@ angular.module(moduleName, [
     'ui.router',
     coreModule,
     authModule
-]).config(routes) // Route configuration
+])
+
+//Application configuration settings
+.constant('app.SETTINGS', {
+    app_title: window.cfg_app_title,
+    app_version: window.cfg_app_version,
+    root_path: window.cfg_root_path
+})
+
+//UI Route configuration
+.config(routes)
+
+//Icon configuration
+.config(icons)
+
 .controller('app.layout.MasterLayoutController', [ '$scope', function($scope) {
 }]);
 
