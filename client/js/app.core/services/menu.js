23567de7d1aa7a0598c8a5d61ddbbd86cab15b73
vamship <vamshi.ponnapalli@gmail.com>
2016-02-22 20:21:58 -0500

    Added menu service

---
+++ client/js/app.core/services/menu.js
@@ -0,0 +1,137 @@
+'use strict';
+
+var _clone = require('clone');
+
+/**
+ * Returns a menu type that represents a single menu item with zero or more
+ * child menu items associated with it. Each menu item has support for multiple
+ * targets (urls, ui router states, etc), icons, and permissions based
+ * rendering.
+ */
+module.exports = [ '$state', 'app.core.user', 'app.core.utils', function($state, user, utils) {
+
+     /**
+      * Represents a single menu item that can be a part of a hierarchical system
+      * of menus. The menu options must include a title property, but all other
+      * properties are optional. Supported options include:
+      *
+      *  - title: The menu title
+      *  - position: The position of the menu item (defaults to 0) 
+      *  - routeState: The state value of the link (if ui router is being used). Defaults to ''
+      *  - routeParams: Parameters used to generate the ui route. Defaults to {}
+      *  - link: A hyperlink to a resource (non ui router). Defaults to '#'
+      *  - fontSet: The font set to use when rendering a font based svg icon. Defaults to 'angular-material'
+      *  - iconName: The name of the svg icon to use
+      *  - roles: An array of roles for which the menu will be displayed. Defaults to ['*']
+      *
+      * @module app.core.menu
+      * @class MenuItem
+      * @constructor
+      * @constructor 
+      * @param {Object} menuOptions An options object that describes the
+      *              menu
+      */
+    function MenuItem(options) {
+        if(!options || options instanceof Array || typeof options !== 'object') {
+            throw new Error('Invalid menu options specified (arg #1)');
+        }
+
+        if(typeof options.title !== 'string' || options.title.length <= 0) {
+            throw new Error('Menu options does not define a valid title property (menuOptions.title)');
+        }
+
+        this.title = options.title;
+        this.position = utils.applyDefaultIfNotNumber(options.position, 0, true);
+        this.routeState = utils.applyDefaultIfNotString(options.routeState, '', true);
+        this.routeParams = utils.applyDefaultIfNotObject(options.routeParams, {});
+        this.link = utils.applyDefaultIfNotString(options.link, '#');
+        this.fontSet = utils.applyDefaultIfNotString(options.fontSet, 'angular-material');
+        this.iconName = utils.applyDefaultIfNotString(options.iconName, '');
+        this.roles = utils.applyDefaultIfNotArray(options.roles, [ '*' ]);
+        this.childItems = [];
+
+        if(options.childItems instanceof Array) {
+            for(var index = 0; index<options.childItems.length; index++) {
+                var childOptions = options.childItems[index];
+                var childMenu = this.addChildItem(childOptions);
+            }
+        }
+    }
+
+    /**
+     * Adds a child item to the current menu item.
+     *
+     * @module app.core.menu
+     * @class MenuItem
+     * @method addChildItem
+     * @param {Object} item A hash that defines the menu item, or a previously
+     *          defined menu item object. If a hash is specified, a new menu item
+     *          will be created using the hash.
+     * @return {Object} A reference to the newly added menu item.
+     */
+    MenuItem.prototype.addChildItem = function(item) {
+        if(!(item instanceof MenuItem)) {
+            item = new MenuItem(item);
+        }
+        this.childItems.push(item);
+        return item;
+    };
+
+    /**
+     * Clears all child items from the current menu item.
+     *
+     * @module app.core.menu
+     * @class MenuItem
+     * @method clearChildItems
+     */
+    MenuItem.prototype.clearChildItems = function() {
+        this.childItems.splice(0, this.childItems.length);
+    };
+
+    /**
+     * Determines whether or not the menu item can be rendered for the current
+     * user.
+     * 
+     * @module app.core.menu
+     * @class MenuItem
+     * @method canRender
+     * @return {Boolean} True if the item can be shown to the user, false
+     *              otherwise.
+     */
+    MenuItem.prototype.canRender = function() {
+        if(this.roles.indexOf('*') >= 0) {
+            return true;
+        }
+        if(!user.isLoggedIn()) {
+            return false;
+        }
+        console.log(user._roles);
+        for(var index=0; index<this.roles.length; index++) {
+            console.log(this.roles[index]);
+            if(user.hasRole(this.roles[index])) {
+                return true;
+            }
+        }
+        return false;
+    };
+
+    /**
+     * Returns a link that can be used when rendering the menu item. This method
+     * leverages ui route information if specified, and if not, defaults to the
+     * link value of the item.
+     *
+     * @class MenuItem
+     * @method getLink
+     * @return {String} A string hyperlink that can be used when rendering the
+     *          menu item.
+     */
+    MenuItem.prototype.getLink = function() {
+        if (typeof this.routeState === 'string' &&
+                            this.routeState.length > 0) {
+            return $state.href(this.routeState, this.routeParams);
+        }
+        return this.link;
+    };
+
+    return MenuItem;
+}];
---
+++ test/client/app.core/services/menu-spec.js
@@ -0,0 +1,422 @@
+/* jshint expr:true */
+/* global alert:true */
+
+var _angular = require('angular');
+var _ngMocks = require('angular-mocks');
+var _chai = require('chai');
+var _sinon = require('sinon');
+var _sinonChai = require('sinon-chai');
+var _chaiAsPromised = require('chai-as-promised');
+var _module = 'app.core';
+
+_chai.use(_sinonChai);
+_chai.use(_chaiAsPromised);
+var expect = _chai.expect;
+
+describe('app.core.menu: ', function() {
+    'use strict';
+
+    var $stateMock = null;
+    var userMock = null;
+    var service = null;
+
+    function _createDefaultMenu(options) {
+        options = options || {
+            title: 'menu1'
+        };
+        return new service(options);
+    }
+
+    function _getHierarchicalOptions() {
+        return {
+            title: 'item-1',
+            roles: ['*'],
+            childItems: [{
+                title: 'item-11',
+                roles: ['*'],
+                childItems: [{
+                    title: 'item-111',
+                    roles: ['manager']
+                }, {
+                    title: 'item-112',
+                    roles: ['user']
+                }, {
+                    title: 'item-113',
+                    roles: ['user', 'manager']
+                }]
+            }, {
+                title: 'item-12',
+                roles: ['user'],
+                childItems: [{
+                    title: 'item-121',
+                    roles: ['manager']
+                }, {
+                    title: 'item-122',
+                    roles: ['manager']
+                }]
+            }, {
+                title: 'item-13',
+                roles: ['admin'],
+                childItems: []
+            }]
+        };
+    }
+
+    beforeEach(function() {
+        $stateMock = {
+            __url: '',
+            href: function() {}
+        };
+
+        _sinon.stub($stateMock, 'href', function(state, params) {
+            return $stateMock.__url;
+        });
+
+        userMock = {
+            _isLoggedIn: false,
+            _roles: [],
+            hasRole: function(role) {
+                return userMock._roles.indexOf(role.toLowerCase()) >= 0;
+            },
+            isLoggedIn: function() {
+                return userMock._isLoggedIn;
+            }
+        };
+    });
+
+    beforeEach(angular.mock.module(_module));
+    beforeEach(angular.mock.module(['$provide', function($provide) {
+        $provide.value('$state', $stateMock);
+        $provide.value('app.core.user', userMock);
+    }]));
+    beforeEach(inject(['app.core.menu', function(injectedService) {
+        service = injectedService;
+    }]));
+
+    describe('[init]', function() {
+        it('should define the necessary fields and methods', function() {
+            expect(service).to.be.a('function');
+        });
+    });
+
+    describe('[ctor]', function() {
+        it('should throw an error if invoked without a valid menu options object', function() {
+            var error = 'Invalid menu options specified (arg #1)';
+            function invokeMethod(options) {
+                return function() {
+                    return new service(options);
+                };
+            }
+
+            expect(invokeMethod(undefined)).to.throw(error);
+            expect(invokeMethod(null)).to.throw(error);
+            expect(invokeMethod('abc')).to.throw(error);
+            expect(invokeMethod(123)).to.throw(error);
+            expect(invokeMethod(true)).to.throw(error);
+            expect(invokeMethod([])).to.throw(error);
+            expect(invokeMethod(function() {})).to.throw(error);
+        });
+
+        it('should throw an error if the menu options does not define a valid menu title', function() {
+            var error = 'Menu options does not define a valid title property (menuOptions.title)';
+
+            function invokeMethod(title) {
+                return function() {
+                    var options = {
+                        title: title
+                    };
+                    return new service(options);
+                };
+            }
+
+            expect(invokeMethod(undefined)).to.throw(error);
+            expect(invokeMethod(null)).to.throw(error);
+            expect(invokeMethod('')).to.throw(error);
+            expect(invokeMethod(123)).to.throw(error);
+            expect(invokeMethod(true)).to.throw(error);
+            expect(invokeMethod({ })).to.throw(error);
+            expect(invokeMethod([])).to.throw(error);
+            expect(invokeMethod(function() {})).to.throw(error);
+        });
+
+        it('should return an object with required properties when invoked with valid options', function() {
+            var title = 'menu1';
+
+            var menu = new service({
+                title: title
+            });
+
+            expect(menu).to.be.an('object');
+            expect(menu).to.have.property('title', title);
+            expect(menu).to.have.property('position').and.to.be.a('number');
+
+            expect(menu).to.have.property('routeState').and.to.be.a('string');
+            expect(menu).to.have.property('routeParams').and.to.be.an('object');
+            expect(menu).to.have.property('link').and.to.be.a('string');
+
+            expect(menu).to.have.property('fontSet').and.to.be.a('string');
+            expect(menu).to.have.property('iconName').and.to.be.a('string');
+
+            expect(menu).to.have.property('roles').and.to.be.an('array');
+            expect(menu).to.have.property('childItems').and.to.be.an('array');
+
+            expect(menu).to.have.property('addChildItem').and.to.be.a('function');
+            expect(menu).to.have.property('clearChildItems').and.to.be.a('function');
+            expect(menu).to.have.property('canRender').and.to.be.a('function');
+            expect(menu).to.have.property('getLink').and.to.be.a('function');
+        });
+
+        it('should apply default values to the menu properties when none are specified via the menu options', function() {
+            var title = 'menu1';
+
+            var menu = new service({
+                title: title
+            });
+
+            expect(menu.position).to.equal(0);
+
+            expect(menu.routeState).to.equal('');
+            expect(menu.routeParams).to.deep.equal({});
+            expect(menu.link).to.equal('#');
+
+            expect(menu.fontSet).to.equal('angular-material');
+            expect(menu.iconName).to.equal('');
+
+            expect(menu.roles).to.deep.equal(['*']);
+            expect(menu.childItems).to.deep.equal([]);
+        });
+
+        it('should use the values specified in the menu options to initialize the menu', function() {
+            var options = {
+                title: 'menu2',
+                position: 23,
+                routeState: 'home.default',
+                routeParams: {
+                    foo: 'bar'
+                },
+                link: 'http://www.google.com',
+                fontSet: 'ba ba ba',
+                iconName: 'banana',
+                roles: ['admin', 'user']
+            };
+
+            var menu = new service(options);
+
+            for (var prop in options) {
+                var propVal = options[prop];
+                if (typeof propVal === 'object') {
+                    expect(menu[prop]).to.deep.equal(propVal);
+                } else {
+                    expect(menu[prop]).to.equal(options[prop]);
+                }
+            }
+        });
+
+        it('should create a hierarchical menu structure based on the specified options', function() {
+            var options = _getHierarchicalOptions();
+
+            function checkItemRecursive(item, options) {
+                if (!(options.childItems instanceof Array)) {
+                    return;
+                }
+                expect(item.title).to.equal(options.title);
+                expect(item.childItems).to.have.length(options.childItems.length);
+                for (var index = 0; index < item.childItems.length; index++) {
+                    var childItem = item.childItems[index];
+                    var childOptions = options.childItems[index];
+                    checkItemRecursive(childItem, childOptions);
+                }
+            }
+
+            var menu = new service(options);
+            checkItemRecursive(menu, options);
+        });
+    });
+
+    describe('Menu.addChildItem()', function() {
+        it('should throw an error if invoked without a valid menu options object', function() {
+            var error = 'Invalid menu options specified (arg #1)';
+
+            function invokeMethod(options) {
+                return function() {
+                    var menu = _createDefaultMenu();
+                    return menu.addChildItem(options);
+                };
+            }
+
+            expect(invokeMethod()).to.throw(error);
+            expect(invokeMethod(null)).to.throw(error);
+            expect(invokeMethod('abc')).to.throw(error);
+            expect(invokeMethod(123)).to.throw(error);
+            expect(invokeMethod(true)).to.throw(error);
+            expect(invokeMethod([1, 2, 3])).to.throw(error);
+            expect(invokeMethod(function() {})).to.throw(error);
+        });
+
+        it('should throw an error if the menu options object does not define a title property', function() {
+            var error = 'Menu options does not define a valid title property (menuOptions.title)';
+
+            function invokeMethod(title) {
+                return function() {
+                    var options = {
+                        title: title
+                    };
+                    var menu = _createDefaultMenu();
+                    return menu.addChildItem(options);
+                };
+            }
+
+            expect(invokeMethod()).to.throw(error);
+            expect(invokeMethod(null)).to.throw(error);
+            expect(invokeMethod('')).to.throw(error);
+            expect(invokeMethod(123)).to.throw(error);
+            expect(invokeMethod(true)).to.throw(error);
+            expect(invokeMethod({
+                foo: 'bar'
+            })).to.throw(error);
+            expect(invokeMethod([1, 2, 3])).to.throw(error);
+            expect(invokeMethod(function() {})).to.throw(error);
+        });
+
+        it('should return a menu item object when invoked', function() {
+            var title = 'menu2';
+            var menu = _createDefaultMenu();
+            var ret = menu.addChildItem({
+                title: title
+            });
+
+            expect(ret).to.be.an.instanceof(service);
+        });
+
+        it('should add the returned item to the items menu of the parent', function() {
+            var title = 'menu2';
+            var menu = _createDefaultMenu();
+
+            expect(menu.childItems).to.have.length(0);
+
+            var ret = menu.addChildItem({
+                title: title
+            });
+
+            expect(menu.childItems).to.have.length(1);
+            expect(menu.childItems[0]).to.equal(ret);
+        });
+
+        it('should accept a fully defined menu object as the an argument, and add it directly to the child items', function() {
+            var menu = _createDefaultMenu();
+            var childMenu = _createDefaultMenu({
+                title: 'child'
+            });
+
+            expect(menu.childItems).to.have.length(0);
+
+            var ret = menu.addChildItem(childMenu);
+
+            expect(menu.childItems).to.have.length(1);
+            expect(ret).to.equal(childMenu);
+            expect(menu.childItems[0]).to.equal(ret);
+        });
+    });
+
+    describe('Menu.clearChildItems()', function() {
+        it('should have no effect if invoked when there are no child items', function() {
+            var menu = _createDefaultMenu();
+
+            expect(menu.childItems).to.have.length(0);
+            menu.clearChildItems();
+            expect(menu.childItems).to.have.length(0);
+        });
+
+        it('should remove all child items from the menu when invoked', function() {
+            var childItemCount = 10;
+            var menu = _createDefaultMenu();
+
+            for (var index = 0; index < childItemCount; index++) {
+                menu.addChildItem({
+                    title: 'item #' + index
+                });
+            }
+
+            expect(menu.childItems).to.have.length(childItemCount);
+            menu.clearChildItems();
+            expect(menu.childItems).to.have.length(0);
+        });
+    });
+
+    describe('Menu.canRender()', function() {
+        it('should return true if the menu item is public', function() {
+            userMock._isLoggedIn = false;
+            var menu = _createDefaultMenu({
+                title: 'menu1',
+                roles: ['*']
+            });
+
+            expect(menu.canRender()).to.be.true;
+        });
+
+        it('should return false if the menu item is not public and the user is not logged in', function() {
+            userMock._isLoggedIn = false;
+            var menu = _createDefaultMenu({
+                title: 'menu1',
+                roles: ['user']
+            });
+
+            expect(menu.canRender()).to.be.false;
+        });
+
+        it('should return true only if the user\'s role matches the role on the menu', function() {
+            userMock._isLoggedIn = true;
+            userMock._roles.push('user');
+            var menu = _createDefaultMenu({
+                title: 'menu1',
+                roles: ['user']
+            });
+
+            expect(menu.canRender()).to.be.true;
+        });
+
+        it('should return false if the user\'s role does not match the role on the menu', function() {
+            userMock._isLoggedIn = true;
+            userMock._roles.push('user');
+            var menu = _createDefaultMenu({
+                title: 'menu1',
+                roles: ['manager']
+            });
+
+            expect(menu.canRender()).to.be.false;
+        });
+    });
+
+    describe('Menu.getLink()', function() {
+        it('should invoke the state service to determine the ui route url if the item has a ui route', function() {
+            var options = {
+                title: 'menu1',
+                routeState: 'abcd.1234',
+                routeParams: {
+                    first: '1',
+                    second: '2'
+                }
+            };
+            var menu = _createDefaultMenu(options);
+            var expectedLink = '#/abcd/1234';
+            $stateMock.__url = expectedLink;
+
+            expect($stateMock.href).to.not.have.been.called;
+            var link = menu.getLink();
+            expect($stateMock.href).to.have.been.calledOnce;
+            expect($stateMock.href).to.have.been.calledWith(options.routeState, options.routeParams);
+            expect(link).to.equal(expectedLink);
+        });
+
+        it('should return the value of the link property if no ui route has been specified', function() {
+            var options = {
+                title: 'menu1',
+                link: 'http://www.google.com'
+            };
+            var menu = _createDefaultMenu(options);
+
+            expect(menu.getLink()).to.equal(options.link);
+        });
+    });
+});
+
